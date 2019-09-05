8ab81f0ca718fa4333894b470de4064cb0598fdb
Luiz Gustavo Rios Moço <luiz.moco@zema.com>
2019-09-04 22:57:20 -0300

    Correcting routes module reference

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.use((req, res, next) => {
 
 app.use(cors())
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
-app.use(require('./routes'))
+app.use(require('./src/routes'))
 
 server.listen(3333)
